fffe733bcaecb5c3bfc9dc14c325a03ccc209171
ChemicalLuck <j.tebbett@outlook.com>
2023-01-19 13:58:13 +0000

    Added packages to install.sh, using xargs to parse files

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
-#!/bin/zsh
+#!/bin/bash
 
 echo "Installing Rust..."; 
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
-packages=""
-while read line
-do
-    cargo install $line
-done < ~/.dotfiles/rust_modules
+echo "Installing Crates..."
+xargs cargo install <rust_modules
+
+echo "Installing Packages..."
+xargs sudo apt-get install <packages
+
